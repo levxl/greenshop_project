b1db2a283feac5284930edc81ed6325a02b7a495
levxl <levdemin238@gmail.com>
2022-05-21 15:22:47 +0600

    first commit

--- django_project/store/views.py
+++ django_project/store/views.py
@@ -4,20 +4,10 @@ from django.views.generic.base import View
 from django.views.generic import ListView
 
 
-def home(request):
-    return render(request, 'store/flowers_list.html')
-
-def product(request):
-    return render(request, 'store/store_doc.html')
-
 class CategoryFilter:
     def get_category(self):
         return Category.objects.all()
 
-class FlowersView(View):
-    def get(self, request):
-        flowers = Flowers.objects.all()
-        return render(request, "store/store_doc.html", {"store_doc" : flowers})
 
 class FlowersListView(CategoryFilter, ListView):
     model = Flowers
@@ -37,9 +27,8 @@ class Search(ListView):
         return Flowers.objects.filter(title__icontains=self.request.GET.get("q"))
 
 class FlowersView(ListView):
-    def get(self, request):
-        flowers = Flowers.objects.all()
-        return render(request, "store/store_doc.html", {"store_doc" : flowers})
+    model = Flowers
+    queryset = Flowers.objects.all()
 
 class AddReview(View):
     def post(self, request, pk):
