929654db5805aecbffea577da8bd5579574bab7f
levxl <levdemin238@gmail.com>
2022-05-22 18:40:11 +0600

    frist commit

--- django_project/store/models.py
+++ django_project/store/models.py
@@ -45,3 +45,29 @@ class Reviews(models.Model):
     class Meta:
         verbose_name = "Отзыв"
         verbose_name_plural = "Отзывы"
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return f'{self.value}'
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+        ordering = ["-value"]
+
+
+class Rating(models.Model):
+    """Рейтинг"""
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    flowers = models.ForeignKey(Flowers, on_delete=models.CASCADE, verbose_name="фильм", related_name="ratings")
+
+    def __str__(self):
+        return f"{self.star} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
--- django_project/store/views.py
+++ django_project/store/views.py
@@ -20,12 +20,38 @@ class FlowersListView(CategoryFilter, ListView):
     model = Flowers
     queryset = Flowers.objects.all()
 
+
+  
+
 class FlowersDetailView(View):
     def get(self, requset, slug):
         flowersget = Flowers.objects.get(url=slug)
         flowersall = Flowers.objects.all()
         return render(requset, "store/store_detail.html", {"flowers": flowersall, 
         "flowersget": flowersget })
+
+class AddStarRating(View):
+    """Добавление рейтинга фильму"""
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def post(self, request):
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            Rating.objects.update_or_create(
+                ip=self.get_client_ip(request),
+                movie_id=int(request.POST.get("movie")),
+                defaults={'star_id': int(request.POST.get("star"))}
+            )
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=400)
 
 class Search(ListView):
     paginate_by = 3
